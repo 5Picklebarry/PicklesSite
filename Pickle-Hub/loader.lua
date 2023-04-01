1c4e074a7d6cd85d425ba9316ad5ea5e48266f2b
PickleSons <78266908+5Picklebarry@users.noreply.github.com>
2023-04-01 16:57:10 -0400

    Add files via upload

---
+++ Pickle-Hub/loader.lua
@@ -0,0 +1,18 @@
+--[[
+This is the Pickle Hub Loader.
+Currently only 4 games supported.
+]]
+
+if game.PlaceId == 5827078246 then
+    loadstring(game:HttpGet(("https://raw.githubusercontent.com/5Picklebarry/Scripts/main/Pickle-Hub/Coins-Hero-Simulator.lua")))()
+elseif game.PlaceId == 12400504665 then
+    loadstring(game:HttpGet(("https://raw.githubusercontent.com/5Picklebarry/Scripts/main/Pickle-Hub/Hood-Arena.lua")))()
+    elseif game.PlaceId == 662417684 then
+    loadstring(game:HttpGet(("https://raw.githubusercontent.com/5Picklebarry/Scripts/main/Pickle-Hub/Lucky-Blocks-Battlegrounds.lua")))()
+    elseif game.PlaceId == 10704789056 then
+    loadstring(game:HttpGet(("https://raw.githubusercontent.com/5Picklebarry/Scripts/main/Pickle-Hub/Drive-World.lua")))()
+    elseif game.PlaceId == 11746859781 then
+    loadstring(game:HttpGet(("https://raw.githubusercontent.com/5Picklebarry/Scripts/main/Pickle-Hub/Bubble-Gum-Clicker.lua")))()
+    else
+    warn("Pickle Hub is not supported in this game.")
+end
